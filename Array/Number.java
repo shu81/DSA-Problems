49d8404707219d8c708e2a3753dff1c12a23e35b
shu81 <shubhamkadamsk121@gmail.com>
2025-02-27 22:59:47 +0530

    Find arrya element index

---
+++ Array/Number.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Number{
+	public static void main(String args []){
+		
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int number[] = new int [size];
+		
+		//input
+		for(int i =0; i<size; i++){
+			number[i] = sc.nextInt();
+		}
+		
+		int x= sc.nextInt();
+		
+		//output
+		for(int i=0; i<number.length; i++){
+			if(number[i] == x)
+			System.out.println("x number at index : "+i);
+		}
+		
+	}
+}
